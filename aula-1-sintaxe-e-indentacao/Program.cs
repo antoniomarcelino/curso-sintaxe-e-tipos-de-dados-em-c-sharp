142a997f3a0fbaabd653ec4a3c406c126c1d8aee
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-11-21 20:12:02 -0300

    usando-namespaces

--- aula-1-sintaxe-e-indentacao/Program.cs
+++ aula-1-sintaxe-e-indentacao/Program.cs
@@ -1,1 +1,3 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using aula_1_sintaxe_e_indentacao.Models;
+Pessoa p = new Pessoa();
+//aula_1_sintaxe_e_indentacao.Models.Pessoa p = new aula_1_sintaxe_e_indentacao.Models.Pessoa();
