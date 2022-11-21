8a7c65ffed5d6bef1c2659b1e4d56501788291bf
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-11-21 20:34:45 -0300

    convencoes-cases-no-codigo

--- aula-1-sintaxe-e-indentacao/Program.cs
+++ aula-1-sintaxe-e-indentacao/Program.cs
@@ -3,4 +3,5 @@ Pessoa pessoa1 = new Pessoa();
 pessoa1.Nome = "Antonio";
 pessoa1.Idade = 28;
 pessoa1.Apresentar();
+Pessoa pessoaFisicaRepresentacao = new Pessoa();
 //aula_1_sintaxe_e_indentacao.Models.Pessoa p = new aula_1_sintaxe_e_indentacao.Models.Pessoa();
