65d6d641b7e1ca7d5d9add980ad160699fbeaf7a
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-12-01 10:30:49 -0300

    manipulando-variaveis

--- aula-2-tipos-de-dados/Program.cs
+++ aula-2-tipos-de-dados/Program.cs
@@ -2,14 +2,18 @@
 
 string apresentacao = "Olá, seja bem vindo";
 int quantidade = 1;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+
+quantidade = 10;
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
 double altura = 1.80;
 decimal preco = 1.80M;
 bool condicao = true;
-Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
-Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variável preco: " + preco);
-Console.WriteLine("Valor da variável condicao: " +  condicao);
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condicao: " +  condicao);
 
 
 // Pessoa pessoa1 = new Pessoa();
