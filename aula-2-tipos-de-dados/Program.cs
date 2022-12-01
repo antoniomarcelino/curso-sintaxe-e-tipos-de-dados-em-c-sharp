d28a4b61bc38b03b9fd53b02e2c7ea06ab5941a3
antoniomarcelino <antoniomarcelinodev@gmail.com>
2022-12-01 10:26:37 -0300

    declarando-os-tipos-de-variaveis

---
+++ aula-2-tipos-de-dados/Program.cs
@@ -0,0 +1,20 @@
+﻿using aula_1_sintaxe_e_indentacao.Models;
+
+string apresentacao = "Olá, seja bem vindo";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80M;
+bool condicao = true;
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+Console.WriteLine("Valor da variável preco: " + preco);
+Console.WriteLine("Valor da variável condicao: " +  condicao);
+
+
+// Pessoa pessoa1 = new Pessoa();
+// pessoa1.Nome = "Antonio";
+// pessoa1.Idade = 28;
+// pessoa1.Apresentar();
+// Pessoa pessoaFisicaRepresentacao = new Pessoa();
+//aula_1_sintaxe_e_indentacao.Models.Pessoa p = new aula_1_sintaxe_e_indentacao.Models.Pessoa();
